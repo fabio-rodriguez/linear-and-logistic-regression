b99d9fb895e565c1629099359d5c02b272ee2a15
Fabio Rodríguez <32336842+fabio-rodriguez@users.noreply.github.com>
2020-10-03 01:23:22 +0200

    Add files via upload

---
+++ naive_bayes.R
@@ -0,0 +1,94 @@
+# Generating train and test set
+library(caret)
+library(dplyr)
+library(ggplot2)
+library(dslabs)
+
+data(heights)
+y <- heights$height
+set.seed(2)
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+train_set <- heights %>% slice(-test_index)
+test_set <- heights %>% slice(test_index)
+
+# Estimating averages and standard deviations
+params <- train_set %>%
+  group_by(sex) %>%
+  summarize(avg = mean(height), sd = sd(height))
+params
+
+# Estimating the prevalence
+pi <- train_set %>% summarize(pi=mean(sex=="Female")) %>% pull(pi)
+pi
+
+# Getting an actual rule
+x <- test_set$height
+f0 <- dnorm(x, params$avg[2], params$sd[2])
+f1 <- dnorm(x, params$avg[1], params$sd[1])
+p_hat_bayes <- f1*pi / (f1*pi + f0*(1 - pi))
+
+
+### Improving sensitivity by increasing the prevalens pi=0.5
+# Computing sensitivity
+y_hat_bayes <- ifelse(p_hat_bayes > 0.5, "Female", "Male")
+sensitivity(data = factor(y_hat_bayes), reference = factor(test_set$sex))
+
+# Computing specificity
+specificity(data = factor(y_hat_bayes), reference = factor(test_set$sex))
+
+# Changing the cutoff of the decision rule
+p_hat_bayes_unbiased <- f1 * 0.5 / (f1 * 0.5 + f0 * (1 - 0.5))
+y_hat_bayes_unbiased <- ifelse(p_hat_bayes_unbiased > 0.5, "Female", "Male")
+sensitivity(data = factor(y_hat_bayes_unbiased), reference = factor(test_set$sex))
+specificity(data = factor(y_hat_bayes_unbiased), reference = factor(test_set$sex))
+
+# Draw plot
+qplot(x, p_hat_bayes_unbiased, geom = "line") +
+  geom_hline(yintercept = 0.5, lty = 2) +
+  geom_vline(xintercept = 67, lty = 2)
+
+
+
+###QDA
+# Load data
+data("mnist_27")
+
+# Estimate parameters from the data
+params <- mnist_27$train %>%
+  group_by(y) %>%
+  summarize(avg_1 = mean(x_1), avg_2 = mean(x_2),
+            sd_1 = sd(x_1), sd_2 = sd(x_2),
+            r = cor(x_1, x_2))
+
+# Contour plots
+mnist_27$train %>% mutate(y = factor(y)) %>%
+  ggplot(aes(x_1, x_2, fill = y, color = y)) +
+  geom_point(show.legend = FALSE) +
+  stat_ellipse(type="norm", lwd = 1.5)
+
+# Fit model
+library(caret)
+train_qda <- train(y ~., method = "qda", data = mnist_27$train)
+# Obtain predictors and accuracy
+y_hat <- predict(train_qda, mnist_27$test)
+confusionMatrix(data = y_hat, reference = mnist_27$test$y)$overall["Accuracy"]
+
+# Draw separate plots for 2s and 7s
+mnist_27$train %>% mutate(y = factor(y)) %>%
+  ggplot(aes(x_1, x_2, fill = y, color = y)) +
+  geom_point(show.legend = FALSE) +
+  stat_ellipse(type="norm") +
+  facet_wrap(~y)
+
+
+###LDA: Is QDA but taking the same mean and sd for all predictors
+params <- mnist_27$train %>%
+  group_by(y) %>%
+  summarize(avg_1 = mean(x_1), avg_2 = mean(x_2),
+            sd_1 = sd(x_1), sd_2 = sd(x_2),
+            r = cor(x_1, x_2))
+params <- params %>% mutate(sd_1 = mean(sd_1), sd_2 = mean(sd_2), r = mean(r))
+train_lda <- train(y ~., method = "lda", data = mnist_27$train)
+y_hat <- predict(train_lda, mnist_27$test)
+confusionMatrix(data = y_hat, reference = mnist_27$test$y)$overall["Accuracy"]
+
